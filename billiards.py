74f2a05646e6c2797b4169b4faf1b7e539a97cd1
pulkitag <pulkitag@berkeley.edu>
2015-11-27 14:36:17 -0800

    Added support for testing billiards layer

--- billiards.py
+++ billiards.py
@@ -12,12 +12,14 @@ import dataio as dio
 import pdb
 import Queue
 import glog
+import matplotlib.pyplot as plt
+from matplotlib import cm as cmap
 
 def wrapper_fetch_data(args):
 	rndSeed, params = args
 	params['randSeed'] = rndSeed
 	ds  = dio.DataSaver(**params)	
-	ims = ds.fetch(params['glimpseSz'])
+	ims = ds.fetch(params['glimpseSz'], params['imSz'])
 	#imNew = []
 	#for imBalls, f, p in ims:
 	#	imB = []
@@ -42,8 +44,11 @@ class DataFetchLayer(caffe.Layer):
 		parser.add_argument('--mxSeqLen', default=100, type=int)
 		parser.add_argument('--mnForce',  default=1e+3, type=float)
 		parser.add_argument('--mxForce',  default=1e+5, type=float)
-		parser.add_argument('--isRect', dest='isRect', action='store_true')
+		parser.add_argument('--isRect', dest='isRect', action='store_true', default=True)
 		parser.add_argument('--no-isRect', dest='isRect', action='store_false')
+		parser.add_argument('--whiteMean',    dest='whiteMean', action='store_true', default=False)
+		parser.add_argument('--no-whiteMean', dest='whiteMean', action='store_false')
+		parser.add_argument('--scale',  default=1.0, type=float)
 		parser.add_argument('--wTheta' ,  default=30,   type=float)
 		parser.add_argument('--wThick' ,  default=30,   type=float)
 		parser.add_argument('--mxWLen' ,  default=600, type=int)
@@ -55,7 +60,14 @@ class DataFetchLayer(caffe.Layer):
 		parser.add_argument('--glimpseSz', default=512, type=int)
 		parser.add_argument('--lookAhead',  default=10, type=int)
 		parser.add_argument('--history',  default=4, type=int)
-		parser.add_argument('--ncpu',   default=4, type=int)
+		parser.add_argument('--ncpu',   default=6, type=int)
+		#Whether to have position instead of velocity labels
+		parser.add_argument('--posLabel', dest='posLabel', action='store_true', default=False)
+		#Whether to have position and velocity as labels or not
+		parser.add_argument('--posVel', dest='posVel', action='store_true', default=False)
+		#Whether to model forces or not
+		parser.add_argument('--isForce', dest='isForce', action='store_true', default=True)
+		parser.add_argument('--no-isForce', dest='isForce', action='store_false')
 		args   = parser.parse_args(argsStr.split())
 		print('Using World Config:')
 		pprint.pprint(args)
@@ -65,19 +77,18 @@ class DataFetchLayer(caffe.Layer):
 		print ('STARTING SETUP')
 		#Get the parameters
 		self.params_ = DataFetchLayer.parse_args(self.param_str) 
-		print ('LOC 0')
 		#Shape the output blobs
 		top[0].reshape(self.params_.batchSz, 3 * self.params_.history, 
 											 self.params_.imSz, self.params_.imSz)
-		top[1].reshape(self.params_.batchSz, self.params_.lookAhead, 2, 1)	
-		print ('LOC 1')
+		top[1].reshape(self.params_.batchSz, self.params_.lookAhead, 2, 1)
+		if self.params_.posVel:
+			assert (len(top)==3)
+			top[2].reshape(self.params_.batchSz, self.params_.lookAhead, 2, 1)
 		#Start the pool of workers
 		self.pool_   = Pool(processes=self.params_.ncpu)
-		print ('LOC 1.5')
 		self.jobs_   = deque()	
 		#Make a Queue for storing the game plays
 		self.play_cache_ = Queue.Queue(maxsize=self.params_.batchSz)
-		print ('LOC 2')
 		#The datastreams
 		self.plays_ = []
 		self.plays_len_ = [] #Stores the lenght of the games in #frames
@@ -88,13 +99,22 @@ class DataFetchLayer(caffe.Layer):
 			self.plays_len_.append(0)
 			self.plays_toe_.append(0)
 			self.plays_tfs_.append(0)
-		print ('LOC 3')
 		#Prepare the data and label vectors
 		self.imdata_ = np.zeros((self.params_.batchSz, 3 * self.params_.history, 
 										self.params_.imSz, self.params_.imSz), np.float32) 
 		self.labels_ = np.zeros((self.params_.batchSz, self.params_.lookAhead, 
 										2, 1), np.float32)
-		print ('SETUP DONE') 
+		self.labels2_ = np.zeros((self.params_.batchSz, self.params_.lookAhead, 
+										2, 1), np.float32)
+		#Set the mean and scaling
+		self.preproc  = edict()
+		if self.params_.whiteMean:
+			self.preproc['mu'] = 255
+		else:
+			self.preproc['mu'] = 0
+		self.preproc['scale'] = self.params_.scale	
+	
+		print ('SETUP DONE')
 		#Start loading the data
 		self.prefetch()
 		
@@ -118,17 +138,21 @@ class DataFetchLayer(caffe.Layer):
 		if self.play_cache_.empty():
 			try:
 				data = self.jobs_.get()
-				#data  = wrapper_fetch_data([3, self.params_])
 			except KeyboardInterrupt:
 				print 'Keyboard Interrupt received - terminating'
 				self.pool_.terminate()
 			for d in data:
 				self.play_cache_.put(d)
-			#print ('PUTTING', len(data))	
-			#self.play_cache_.put(data)	
 			self.prefetch()
 		#Return one of the plays from the Queue
 		return self.play_cache_.get()
+
+	def preproc_image(self, im):
+		#Resize and make it in the format ch, h, w
+		im = scm.imresize(im, (self.params_.imSz, self.params_.imSz)).transpose((2,0,1))
+		im = im - self.preproc.mu	
+		im = im * self.preproc.scale
+		return im
 
 	def get_next_sample(self):
 		for j in range(self.params_.batchSz):
@@ -136,28 +160,43 @@ class DataFetchLayer(caffe.Layer):
 			if self.plays_toe_[j] - self.params_.lookAhead < 0:
 				#print ('I AM HERE', j)
 				self.plays_[j]     = self.get_cache_data()	
-				self.plays_len_[j] = len(self.plays_[j][0][0])	
-				self.plays_toe_[j] = len(self.plays_[j][0][0])
-				self.plays_tfs_[j] = 0
+				self.plays_len_[j] = len(self.plays_[j][0][0])
+				if self.params_.isForce:	
+					self.plays_toe_[j] = len(self.plays_[j][0][0])
+					self.plays_tfs_[j] = 0
+				else:
+					self.plays_toe_[j] = len(self.plays_[j][0][0]) - self.params_.lookAhead
+					self.plays_tfs_[j] = self.params_.lookAhead
 			#print len(self.plays_[j])
 			#print len(self.plays_[j][0])
-			imBalls, force, vel = self.plays_[j]
+			imBalls, force, vel, pos = self.plays_[j]
 			#For now just chose the first ball
 			imBall = imBalls[0]
 			for h in range(self.params_.history):
 				stCh = h * 3
 				enCh = stCh + 3
 				#print (stCh, enCh, h) 
-				h = max(0, self.plays_len_[j] - self.plays_toe_[j] + h)
+				h = max(0, self.plays_len_[j] - self.plays_toe_[j] + h - self.params_.history)
 				#print (h)
 				#print (stCh, enCh, h, imBall[h].shape)
-				self.imdata_[j,stCh:enCh,:,:] = scm.imresize(imBall[h], 
-								(self.params_.imSz, self.params_.imSz)).transpose((2,0,1))
+				ylen, xlen, numCh = imBall[h].shape
+				#self.imdata_[j,stCh:enCh,:,:] = scm.imresize(imBall[h], 
+				#				(self.params_.imSz, self.params_.imSz)).transpose((2,0,1))
+				self.imdata_[j, stCh:enCh, :, :] = self.preproc_image(imBall[h])
+				yScale, xScale = float(self.params_.imSz)/ylen, float(self.params_.imSz)/xlen
+				posScale = np.array([xScale, yScale]).reshape((2,1))
 			stLbl = self.plays_tfs_[j]
 			enLbl = stLbl + self.params_.lookAhead
 			for l in range(stLbl, enLbl):
 				#Choose the first ball
-				self.labels_[j,l-stLbl, 0:2] = vel[0:2,l].reshape((2,1))/10000
+				if self.params_.posVel:
+					self.labels_[j,l-stLbl, 0:2]  = pos[0:2,l].reshape((2,1)) * posScale 
+					self.labels2_[j,l-stLbl, 0:2] = vel[0:2,l].reshape((2,1))/10000 * posScale
+				else:
+					if self.params_.posLabel:
+						self.labels_[j,l-stLbl, 0:2] = pos[0:2,l].reshape((2,1)) * posScale 
+					else:
+						self.labels_[j,l-stLbl, 0:2] = vel[0:2,l].reshape((2,1))/10000 * posScale
 			#Update the counts
 			self.plays_tfs_[j] += 1
 			self.plays_toe_[j] -= 1
@@ -173,6 +212,8 @@ class DataFetchLayer(caffe.Layer):
 		glog.info('Waiting for fetch: %f'  %tFetch)
 		top[0].data[...] = self.imdata_[...]
 		top[1].data[...] = self.labels_[...]
+		if self.params_.posVel:
+			top[2].data[...] = self.labels2_[...]
 
 	def backward(self, top, propagate_down, bottom):
 		""" This layer has no backward """
@@ -181,3 +222,42 @@ class DataFetchLayer(caffe.Layer):
 	def reshape(self, bottom, top):
 		""" This layer has no reshape """
 		pass
+
+def vis_im(ax, ims, pos, vel):
+	im = ims[0:3,:,:].transpose((1,2,0))
+	im = np.uint8(im)
+	print im.shape
+	ax.imshow(im)	
+	scale  = 10000
+	deltaT = 0.01
+	x, y = pos[0].squeeze()
+	cIdx = (255.0/40) * np.array(range(40))
+	for i in range(pos.shape[0]):
+		vx, vy = vel[i].squeeze()
+		vx    = vx * scale * deltaT
+		vy    = vy * scale * deltaT
+		x     = x + vx
+		y     = y + vy
+		#print (x,y, vx, vy)
+		ax.plot(round(x), round(y), '.', color=cmap.jet(int(cIdx[i])),  markersize=10) 
+	plt.draw()
+	plt.show()
+
+def test_billiards_layer():
+	net = caffe.Net('test/billiards.prototxt', caffe.TEST)
+	plt.ion()
+	fig = plt.figure()
+	ax = []
+	for p in range(4):
+		ax.append(fig.add_subplot(2,2,p+1))
+	for i in range(10000):
+		data = net.forward(blobs=['data', 'pos', 'vel'])
+		im, pos, vel   = data['data'], data['pos'], data['vel']
+		for p in range(4):	
+			vis_im(ax[p], im[0][p*3:p*3+3], pos[0], vel[0])
+		print ('Waiting ...', i)
+		ip = raw_input()
+		if ip=='q':
+			return
+		for p in range(4):	
+			ax[p].clear()
